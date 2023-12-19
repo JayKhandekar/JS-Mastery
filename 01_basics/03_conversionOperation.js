ba308d9a6ace2cb879367ce14a58ad68d86b3edd
JayKhandekar <117138244+JayKhandekar@users.noreply.github.com>
2023-12-19 09:22:39 +0000

    Conversion of dataTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -37,9 +37,41 @@ let value = 3
 let negValue = -value
 // console.log(negValue)
 
-// console.log(3 + 3)
-// console.log(3 - 3)
-// console.log(3 * 3)
-// console.log(3 / 3)
-// console.log(3 ** 3)
-// console.log(3 % 3)
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3/3);
+// console.log(3**3);
+// console.log(3%3);
+
+let str1 = "Hello"
+let str2 = " Jay"
+
+let str3= str1 + str2
+
+// console.log( str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3+5) * 5 / 7);
+
+
+// console.log(true);
+// console.log(+true) //not recommended coversion
+// console.log(+"")//not recommended conversion
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2  // not good (readability is most important)
+
+
+let gameCounter = 100
+
+++node 01_basics/03_conversionOperation.jsgameCounter;
+
+console.log(gameCounter);
