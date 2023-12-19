f02dc1fcc705f57c6e51d130ae68e4c4388bb007
JayKhandekar <117138244+JayKhandekar@users.noreply.github.com>
2023-12-19 13:54:43 +0000

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log( null > 0);
+// console.log( null < 0);
+// console.log( null == 0);
+// console.log( null != 0);
+// console.log( null >= 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+// working of (==) and (>, <, >=, <=) works differently and have different syntax for working
+
+
+// === strict check
+
+console.log("2" === 2);
