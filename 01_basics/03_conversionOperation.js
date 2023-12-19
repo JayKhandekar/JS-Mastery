4c2287272d0651c5e861bdb225aece20a93ecb1c
JayKhandekar <117138244+JayKhandekar@users.noreply.github.com>
2023-12-19 09:25:05 +0000

    Conversion on Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -65,13 +65,13 @@ let str3= str1 + str2
 // console.log(+"")//not recommended conversion
 
 
-let num1, num2, num3
+// let num1, num2, num3
 
-num1 = num2 = num3 = 2 + 2  // not good (readability is most important)
+// num1 = num2 = num3 = 2 + 2  // not good (readability is most important)
 
 
 let gameCounter = 100
 
-++node 01_basics/03_conversionOperation.jsgameCounter;
+++gameCounter;
 
 console.log(gameCounter);
