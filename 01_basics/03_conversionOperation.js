1500811e6dae4f6a8ef5e11178af154186d98fb7
JayKhandekar <117138244+JayKhandekar@users.noreply.github.com>
2023-12-19 09:04:58 +0000

    Conversion of dataTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,5 +28,18 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log (stringNumber);
-console.log (typeof stringNumber);
+// console.log (stringNumber);
+// console.log (typeof stringNumber);
+
+
+//************************Operations**************************
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(3 + 3)
+// console.log(3 - 3)
+// console.log(3 * 3)
+// console.log(3 / 3)
+// console.log(3 ** 3)
+// console.log(3 % 3)
