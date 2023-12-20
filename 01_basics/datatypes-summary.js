80226669fe22235dcfd702ffa620e38b54ad6198
JayKhandekar <117138244+JayKhandekar@users.noreply.github.com>
2023-12-20 07:01:41 +0000

    data-types summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefinied, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIN = false
+const outsideTemp = null
+let userEmai;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+
+//const bigNumber = 10983798571873987n
+
+
+//Javascript is Dynamically-types language
+
+//Regerence (Non primitive)
+
+//Array , Objects, Functions
+
+
+
+const heros = ["shaktiman", "naagraj", "doga"];// array
+
+let myObj = {
+    name : "jay",
+    age : 20,
+
+} // Object
+
+const myFunction= function(){
+    console.log("Hello world");
+}// Function
+
+
+console.log(typeof bigNumber  );
