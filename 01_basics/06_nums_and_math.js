5e5997d0c75196cd136b95145a24a4ef4d9cc6fa
JayKhandekar <117138244+JayKhandekar@users.noreply.github.com>
2024-11-13 16:45:44 +0000

    Math and Nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,65 @@
+const score = 400;
+// console.log(score);
+
+// console.log(typeof score);
+
+const balance = new Number(100);
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 23.8966;
+
+// console.log(otherNumber.toPrecision(3));
+
+
+const otherNumber1 = 123.8966;
+// console.log(otherNumber1.toPrecision(3));
+
+const otherNumber2 = 1123.8966;
+// console.log(otherNumber2.toPrecision(3));
+
+
+
+
+const hundreds = 1000000;
+
+// console.log(hundreds.toLocaleString());
+
+// console.log(hundreds.toLocaleString("en-IN")); //Indian standard
+
+
+//+++++++++++Maths+++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));console.log(Math.abs(4));console.log(Math.round(4.3));console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,5,6,8,9));
+// console.log(Math.max(4,5,6,8,9));
+
+
+
+console.log(Math.random());// 0<= random value < 1
+
+
+console.log(Math.random()*10 );
+
+console.log(Math.random()*10 + 1 );
+
+console.log((Math.random()*10 )+ 1 );
+
+console.log(Math.floor(Math.random()*10 )+ 1) ; // will give values greater than or eual to 1
+
+
+const min = 10;
+const max = 20;
+
+
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // getting min num greater than min .... important formula
+
